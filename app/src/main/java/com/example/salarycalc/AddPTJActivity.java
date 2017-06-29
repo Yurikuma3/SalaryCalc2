46b2296132377cb35e261b23a3dc1208403c9559
kubotamari <g15919km@gm.tsuda.ac.jp>
2017-06-29 18:34:42 +0900

    戻るを無効にしたりいろいろ。

--- app/src/main/java/com/example/salarycalc/AddPTJActivity.java
+++ app/src/main/java/com/example/salarycalc/AddPTJActivity.java
@@ -5,15 +5,12 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddPTJActivity extends AppCompatActivity {
-
-    //NumberPicker numberPicker;
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +19,14 @@ public class AddPTJActivity extends AppCompatActivity {
     }
 
     public void showData(View v) {
+        //データを表示する
+        setContentView(R.layout.activity_add_ptj);
+        Intent dbIntent = new Intent(AddPTJActivity.this,ShowDataBase.class);
+        startActivity(dbIntent);
+
+    }
+
+    public void dataInput(View v){
         MyOpenHelper helper = new MyOpenHelper(this);
         final SQLiteDatabase db = helper.getWritableDatabase();
 
@@ -30,29 +35,34 @@ public class AddPTJActivity extends AppCompatActivity {
         String jobname = txt_jobname.getText().toString(); // Stringに直す
         String salary = txt_salary.getText().toString();
 
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("ptj_name", jobname);
-        contentValues.put("salary", salary);
-        //long id = db.insert("ptj", jobname, contentValues); // 登録したデータのIDを取得
+        if(jobname.equals("")) {
+            Toast.makeText(AddPTJActivity.this,"バイト先を入力してください。", Toast.LENGTH_SHORT).show();
+        } else if(salary.equals("")) {
+            Toast.makeText(AddPTJActivity.this, "時給を入力してください。", Toast.LENGTH_SHORT).show();
+        } else {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("ptj_name", jobname);
+            contentValues.put("salary", salary);
+            long id = db.insert("ptj", jobname, contentValues); //登録したデータのIDを取得
+            Toast.makeText(AddPTJActivity.this, "登録完了", Toast.LENGTH_SHORT).show();
+            txt_jobname.setText("");
+            txt_salary.setText("");
+        }
 
-        //データを表示する
-        setContentView(R.layout.show_database);
-        Intent dbIntent = new Intent(AddPTJActivity.this,ShowDataBase.class);
-        startActivity(dbIntent);
     }
-    /*
-    private void findViews() {
-        Intent intent = getIntent();
-        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        textView = new TextView(this);
-        /*
-        textView.setTextSize(40);
-        textView.setText(message);
-        */
-/*
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_add_ptj);
-        layout.addView(textView);
+
+    public void toTop(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
-    */
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Disable Back key
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
 }
--- app/src/main/java/com/example/salarycalc/MainActivity.java
+++ app/src/main/java/com/example/salarycalc/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.salarycalc;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.salarycalc.MESSAGE";
@@ -18,35 +19,108 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String s = "ここだよ";
-        TextView textView = (TextView)findViewById(R.id.showShift);
-        textView.setText(s);
+        MyOpenHelperShift helper1 = new MyOpenHelperShift(this);
+        final SQLiteDatabase db = helper1.getWritableDatabase();
 
-        MyOpenHelper helper = new MyOpenHelper(this);
-        SQLiteDatabase db = helper.getReadableDatabase();
+        TextView show = (TextView) findViewById(R.id.show_shift);
 
-        Cursor c = db.query("ptj",new String[] {"ptj_name"}, null,null,null,null,null);
+        Cursor c = db.query("shift", new String[]{"shift_name", "date", "time"}, null, null, null, null, null);
 
         boolean mov = c.moveToFirst();
+        String shifts = "";
+
+        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<Integer> times = new ArrayList<Integer>();
         while (mov) {
-            s += String.format("%s\n", c.getString(0));
+            shifts += String.format("%s 　日付 : %s 　時間 : %d 時間 \n",c.getString(0), c.getString(1), c.getInt(2));
+            names.add(c.getString(0));
+            times.add(c.getInt(2));
+            mov = c.moveToNext();
+        }
+
+        show.setText(shifts);
+
+        // 合計金額を表示
+        MyOpenHelper helper2 = new MyOpenHelper(this);
+        final SQLiteDatabase db2 = helper2.getWritableDatabase();
+
+        TextView showSum = (TextView) findViewById(R.id.lblSum);
+        Cursor cp = db2.query("ptj", new String[]{"ptj_name", "salary"}, null, null, null, null, null);
+
+        boolean mov2 = cp.moveToFirst();
+        String s;
+        int sum=0;
+        while(mov2) {
+            s = cp.getString(0);
+
+            for(int i=0; i<names.size(); i++) {
+                if(s.equals(names.get(i))) {
+                    sum += times.get(i) * cp.getInt(1);
+                }
+            }
+            mov2 = cp.moveToNext();
+        }
+
+       showSum.setText("給料合計： "+sum+"円");
+
+        c.close();
+        cp.close();
+        db.close();
+        db2.close();
+    }
+
+    public void toAddPTJ(View view) {
+        Intent intent = new Intent(this, AddPTJActivity.class);
+        startActivity(intent);
+    }
+
+    public void toAddShift(View view) {
+        Intent intent = new Intent(this, AddShiftActivity.class);
+        startActivity(intent);
+    }
+/*
+    public void updateShift(){
+        //setContentView(R.layout.activity_main);
+
+        MyOpenHelperShift helper = new MyOpenHelperShift(this);
+        final SQLiteDatabase db = helper.getWritableDatabase();
+
+        TextView show = (TextView) findViewById(R.id.show_shift);
+
+        Cursor c = db.query("shift", new String[]{"shift_name", "date", "time"}, null, null, null, null, null);
+
+        boolean mov = c.moveToFirst();
+        String shifts = "";
+
+        while (mov) {
+            shifts += String.format("バイト：%s 　日付 : %s 　時間 : %d 時間 \n",c.getString(0), c.getString(1), c.getInt(2));
             mov = c.moveToNext();
         }
         c.close();
         db.close();
-        textView.setText(s);
-    }
 
-    public void addPTJ(View view) {
-        Intent intent = new Intent(this, AddPTJActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.edit_message);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
+        show.setText(shifts);
+    }
+*/
+    public void dbClear(View v){
+        MyOpenHelper helper = new MyOpenHelper(this);
+        final SQLiteDatabase db = helper.getWritableDatabase();
+        db.delete("ptj", null, null);
+
+        MyOpenHelperShift helpers = new MyOpenHelperShift(this);
+        final SQLiteDatabase db2 = helpers.getWritableDatabase();
+        db2.delete("shift",null, null);
+
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void addShift(View view) {
-        Intent intent = new Intent(this, AddShiftActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Disable Back key
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
